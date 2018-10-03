1256beacf6d4e8af7c16728177989df8c935d12e
aaaaaa2493 <aaaaaa2493@yandex.ru>
2018-10-04 00:58:32 +0300

    Stage 3 completed

--- src/unitConverter/main.kt
+++ src/unitConverter/main.kt
@@ -2,15 +2,48 @@ package unitConverter
 
 import java.util.*
 
+fun getCorrectName(value: Double, measure: String): String {
+    return when(measure) {
+        "meters", "meter", "m" ->
+            if (value == 1.0) "meter" else "meters"
+        "kilometers", "kilometer", "km" ->
+            if (value == 1.0) "kilometer" else "kilometers"
+        "centimeters", "centimeter", "cm" ->
+            if (value == 1.0) "centimeter" else "centimeters"
+        "millimeters", "millimeter", "mm" ->
+            if (value == 1.0) "millimeter" else "millimeters"
+        "miles", "mile", "mi" ->
+            if (value == 1.0) "mile" else "miles"
+        "yards", "yard", "yd" ->
+            if (value == 1.0) "yard" else "yards"
+        "feet", "foot", "ft" ->
+            if (value == 1.0) "foot" else "feet"
+        "inches", "inch", "in" ->
+            if (value == 1.0) "inch" else "inches"
+        else -> "???"
+    }
+}
+
 fun main(args: Array<String>) {
 
     val scanner = Scanner(System.`in`)
 
-    print("Enter a number of kilograms: ")
+    print("Enter a number and a measure of length: ")
 
-    val kilos = scanner.nextInt()
-    val grams = kilos * 1000
+    val value = scanner.nextDouble()
+    val measure = scanner.next().toLowerCase()
 
-    println("$kilos kilograms is $grams grams")
+    val result = value * when(measure) {
+        "meters", "meter", "m" -> 1.0
+        "kilometers", "kilometer", "km" -> 1000.0
+        "centimeters", "centimeter", "cm" -> 0.01
+        "millimeters", "millimeter", "mm" -> 0.001
+        "miles", "mile", "mi" -> 1609.35
+        "yards", "yard", "yd" -> 0.9144
+        "feet", "foot", "ft" -> 0.3048
+        "inches", "inch", "in" -> 0.0254
+        else -> 0.0
+    }
 
+    println("$value ${getCorrectName(value, measure)} is $result ${getCorrectName(result, "m")}")
 }
